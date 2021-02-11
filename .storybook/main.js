cbf340b7518e69ea182168987524a4afa229a1ea
Seba Grochocki <>
2021-02-11 13:26:22 +0100

    add missing config files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,12 @@
-// .storybook/main.js
-
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|mdx)'],
-  addons: ['@storybook/addon-essentials']
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    'storybook-addon-designs'
+  ]
 }
